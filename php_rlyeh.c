f5fa55d9fcf01ed1930003c9ea2259ba5850fa97
Kristina <kristina@10gen.com>
2011-08-10 16:08:43 -0400

    added MadnessException

--- php_rlyeh.c
+++ php_rlyeh.c
@@ -1,12 +1,16 @@
 // include PHP API
 #include <php.h>
+#include <zend_exceptions.h>
 
 // header file we'll create below
 #include "php_rlyeh.h"
 
+zend_class_entry *rlyeh_ce_exception;
+
 // define the function(s) we want to add
 zend_function_entry rlyeh_functions[] = {
   PHP_FE(cthulhu, NULL)
+  PHP_FE(lookAtMonster, NULL)
   { NULL, NULL, NULL }
 };
 
@@ -17,7 +21,7 @@ zend_module_entry rlyeh_module_entry = {
   STANDARD_MODULE_HEADER,
   PHP_RLYEH_EXTNAME,
   rlyeh_functions,
-  NULL,
+  PHP_MINIT(rlyeh),
   NULL,
   NULL,
   NULL,
@@ -26,6 +30,10 @@ zend_module_entry rlyeh_module_entry = {
   STANDARD_MODULE_PROPERTIES
 };
 
+PHP_MINIT_FUNCTION(rlyeh) {
+  rlyeh_init_exception(TSRMLS_C);
+}
+
 // install module
 ZEND_GET_MODULE(rlyeh)
 
@@ -33,4 +41,15 @@ ZEND_GET_MODULE(rlyeh)
 PHP_FUNCTION(cthulhu) {
   // php_printf is PHP's version of printf, it's essentially "echo" from C
   php_printf("In his house at R'lyeh dead Cthulhu waits dreaming.\n");
+}
+
+void rlyeh_init_exception(TSRMLS_D) {
+  zend_class_entry e;
+
+  INIT_CLASS_ENTRY(e, "MadnessException", NULL);
+  rlyeh_ce_exception = zend_register_internal_class_ex(&e, (zend_class_entry*)zend_exception_get_default(TSRMLS_C), NULL TSRMLS_CC);
+}
+
+PHP_FUNCTION(lookAtMonster) {
+  zend_throw_exception(rlyeh_ce_exception, "looked at the monster too long", 1000 TSRMLS_CC);
 }
--- php_rlyeh.h
+++ php_rlyeh.h
@@ -2,3 +2,9 @@
 #define PHP_RLYEH_VERSION "0.01"
 
 PHP_FUNCTION(cthulhu);
+
+PHP_MINIT_FUNCTION(rlyeh);
+
+void rlyeh_init_exception(TSRMLS_D);
+
+PHP_FUNCTION(lookAtMonster);
